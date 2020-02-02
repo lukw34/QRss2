ef9ad6fff75791c245429abf8d42d869bf2bbb54
Lukasz Walewski <lukw34@gmail.com>
2020-02-02 14:01:49 +0100

    Init ts config

---
+++ src/RootApp.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    StatusBar, Text,
+} from 'react-native';
+
+const App = () => (
+    <>
+        <StatusBar barStyle="light-content" />
+        <Text>QRss2</Text>
+    </>
+);
+
+export default App;
